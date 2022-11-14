8a5ee28a80662008d12dc70ab2230bbc0c30082d
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-11-14 15:14:38 +0100

    koniec lekcji

--- Programowanie strukturalne i obiektowe/CollectionConsoleApp/TestCollection.cs
+++ Programowanie strukturalne i obiektowe/CollectionConsoleApp/TestCollection.cs
@@ -25,8 +25,6 @@ namespace CollectionConsoleApp
             {
 
             }
-
-
             */
             int[] arrayOfNumbers = new int[10];
             arrayOfNumbers[5] = 2137;
@@ -34,13 +32,31 @@ namespace CollectionConsoleApp
             Random random = new Random();
             for (int i = 0; i < arrayOfNumbers.Length; i++)
             {
-                arrayOfNumbers[i] = random.Next();
+                arrayOfNumbers[i] = random.Next() % 100;
             }
 
+            /*
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                Console.Write(arrayOfNumbers[i] + ", ");
+            }
+            */
+
+            Console.WriteLine("Liczby w tablicy:");
             foreach (int item in arrayOfNumbers)
             {
                 Console.Write(item + ", ");
+                //item = 5;
             }
+            Console.WriteLine();
+
+            int max = arrayOfNumbers[0];
+            foreach (int number in arrayOfNumbers)
+            {
+                if (number > max)
+                    max = number;
+            }
+            Console.WriteLine("Max w tablicy to: " + max);
 
 
         }
