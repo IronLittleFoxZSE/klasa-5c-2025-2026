fef4b0a58d90891cf579c3555a58574eb25a040b
IronLittleFox <lukasz.felisek@gmail.com>
2023-02-27 14:46:23 +0100

    h

---
+++ Programowanie strukturalne i obiektowe/ExceptionApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExceptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber, secondNumber, divNumber;
+
+            Console.WriteLine("Podaj pierwszą liczbę");
+            firstNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugą liczbę");
+            secondNumber = double.Parse(Console.ReadLine());
+
+            divNumber = firstNumber / secondNumber;
+
+            Console.WriteLine("Wynik dzielenia " + divNumber);
+        }
+    }
+}
