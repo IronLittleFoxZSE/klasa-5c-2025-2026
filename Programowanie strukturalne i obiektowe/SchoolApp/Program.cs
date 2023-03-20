8b757d0a970bafbb326b59bbe7bcf14bae77888d
IronLittleFox <lukasz.felisek@gmail.com>
2023-03-20 10:11:32 +0100

    k

--- Programowanie strukturalne i obiektowe/SchoolApp/Program.cs
+++ Programowanie strukturalne i obiektowe/SchoolApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolApp
 {
@@ -6,7 +7,7 @@ namespace SchoolApp
     {
         static void Main(string[] args)
         {
-           
+            Entities
         }
     }
 }
