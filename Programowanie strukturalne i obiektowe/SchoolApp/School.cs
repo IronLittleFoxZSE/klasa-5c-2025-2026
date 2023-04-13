2a0203f04a9bb41c2cbbf4824d579abb5c3abed9
IronLittleFox <lukasz.felisek@gmail.com>
2023-04-13 11:39:45 +0200

    koniec lekcji

--- Programowanie strukturalne i obiektowe/SchoolApp/School.cs
+++ Programowanie strukturalne i obiektowe/SchoolApp/School.cs
@@ -1,4 +1,5 @@
 ﻿using SchoolApp.Database;
+using SchoolApp.Database.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,27 +14,27 @@ namespace SchoolApp
 
         public void Run()
         {
-            while(true)
+            while (true)
             {
                 ShowMenu();
 
                 Console.WriteLine("Wybierz:");
                 if (!int.TryParse(Console.ReadLine(), out int option))
                     continue;
-                
-                switch(option)
+
+                switch (option)
                 {
                     case 1:
-                        //uzupełnić metodę
+                        AddNewSchoolClass();
                         break;
                     case 2:
-                        //uzupełnić metodę
+                        DeleteSchoolClass();
                         break;
                     case 3:
-                        //usupełnić metodę
+                        ModifySchoolClass();
                         break;
                     case 4:
-                        //uzupełnić metodę
+                        ShowAllSchoolClasses();
                         break;
 
                     case 0:
@@ -56,7 +57,105 @@ namespace SchoolApp
             Console.WriteLine("0. Koniec programu.");
         }
 
+        #region Metody do pracy na tabeli SchoolClasses
 
+        private void AddNewSchoolClass()
+        {
+            Console.WriteLine("Podaj nazwę klasy");
+            string className = Console.ReadLine();
 
+            SchoolClass schoolClass = new SchoolClass()
+            {
+                Name = className
+            };
+
+            schoolDatabase.SchoolClasses.Add(schoolClass);
+            schoolDatabase.SaveChanges();
+        }
+
+        private void ShowAllSchoolClasses()
+        {
+            Console.WriteLine("Wszystkie klasy:");
+            /*
+             select *
+               from SchoolClasses
+            */
+            foreach (SchoolClass schoolClass in schoolDatabase.SchoolClasses)
+            {
+                Console.WriteLine(schoolClass.Id + " " + schoolClass.Name);
+            }
+            Console.ReadKey();
+        }
+
+        private void DeleteSchoolClass()
+        {
+            Console.WriteLine("Podaj id klasy do usunięcia");
+            if (int.TryParse(Console.ReadLine(), out int idToDeleted))
+            {
+                /*
+                 delete SchoolClasses
+                  where id = idToDeleted
+                 */
+
+                /*
+                 select *
+                   from SchoolClasses
+                  where id = idToDeleted
+                 */
+                SchoolClass schoolClassToDeleted = schoolDatabase
+                                                    .SchoolClasses
+                                                    .FirstOrDefault(sc => sc.Id == idToDeleted);
+                if (schoolClassToDeleted != null)
+                {
+                    schoolDatabase.SchoolClasses.Remove(schoolClassToDeleted);
+                    schoolDatabase.SaveChanges();
+                    Console.WriteLine("Kasowanie zakończone sukcesem.");
+                }
+                else
+                {
+                    Console.WriteLine("Brak klasy w bazie to takim id");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Błąd parsowania. Nieprawidłowe dane.");
+            }
+            Console.ReadKey();
+        }
+
+        private void ModifySchoolClass()
+        {
+            Console.WriteLine("Podaj id klasy do modyfikacji");
+            if (int.TryParse(Console.ReadLine(), out int idToModify))
+            {
+                SchoolClass schoolClassToModify = schoolDatabase
+                                                    .SchoolClasses
+                                                    .FirstOrDefault(sc => sc.Id == idToModify);
+                if (schoolClassToModify != null)
+                {
+                    Console.WriteLine("Podaj nową nazwę klasy");
+                    string newName = Console.ReadLine();
+
+                    schoolClassToModify.Name = newName;
+                    schoolDatabase.SaveChanges();
+                    /*
+                     update set Name =  newName
+                    from schoolClassToModify
+                    where id =idToModify
+                     */
+                    Console.WriteLine("Modyfikacja zakończona sukcesem.");
+                }
+                else
+                {
+                    Console.WriteLine("Brak klasy w bazie to takim id");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Błąd parsowania. Nieprawidłowe dane.");
+            }
+            Console.ReadKey();
+        }
+        #endregion
     }
 }
