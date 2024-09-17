66bc79e1ce7bf3d72f07b92f5f946e2e8f3ffddb
f <f@f.pl>
2024-09-17 14:24:06 +0200

    koniec lekcji

---
+++ Programowanie strukturalne i obiektowe/BindingMauiApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace BindingMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Message { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            rotationLabel.Rotation = rotationSlider.Value;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            lab.Text = "Wartość z entry " + Message;
+        }
+    }
+
+}
