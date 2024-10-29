c860366e37a06edf66bd8aa41d40d225497c883a
f <f@f.pl>
2024-10-29 14:24:12 +0100

    dodanie konwerterów

---
+++ Programowanie strukturalne i obiektowe/ConverterMauiApp/Converters/GradeToTextGradeConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterMauiApp.Converters
+{
+    internal class GradeToTextGradeConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value == null
+                || value is not string)
+            {
+                throw new Exception("Błedna wartość");
+            }
+
+            string grade = value as string;
+
+            string textGrade = grade switch
+            {
+                "1" => "Niedostateczny",
+                "2" => "Dopuszczający",
+                "3" => "Dostateczny",
+                "4" => "Dobry",
+                "5" => "Bardzo dobry",
+                "6" => "Celujący",
+                _ => "Błędne dane"
+            };
+
+            return textGrade;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Programowanie strukturalne i obiektowe/ConverterMauiApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace ConverterMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private string grade;
+
+        public string Grade
+        {
+            get { return grade; }
+            set 
+            { 
+                grade = value;
+                OnPropertyChanged();
+                switch(Grade)
+                {
+                    case "1":
+                        TextGrade = "Niedostateczny";
+                        break;
+                    case "2":
+                        TextGrade = "Dopuszczający";
+                        break;
+                    default:
+                        TextGrade = "Nieprawidłowe dane";
+                        break;
+                }
+                //TextGrade = Grade;
+            }
+        }
+
+        private string textGrade;
+        public string TextGrade
+        {
+            get { return textGrade; }
+            set 
+            { 
+                textGrade = value;
+                OnPropertyChanged();
+
+            }
+        }
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
