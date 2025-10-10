c09a500c800e7f8cba85d27b9c772bdcafd971ce
f <f@f.pl>
2025-10-10 14:07:08 +0200

    koniec lekcji

---
+++ Programowanie strukturalne i obiektowe/TabPageMauiApp/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace TabPageMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            TabbedPage tabbedPage = new TabbedPage();
+            tabbedPage.BarTextColor = Colors.Red;
+            tabbedPage.SelectedTabColor = Colors.Blue;
+            tabbedPage.BarBackgroundColor = Colors.Yellow;
+            tabbedPage.Children.Add(new MainPage() { Title = "Strona główna"});
+            tabbedPage.Children.Add(new AboutPage());
+            tabbedPage.Children.Add(new AnotherPage());
+
+            tabbedPage.Children.Add(new NavigationPage(new AnotherPage()));
+
+            /*return new Window(tabbedPage)
+            {
+                TitleBar = new TitleBar()
+                {
+                    BackgroundColor = Colors.Red
+                }
+            };*/
+
+            return new Window(tabbedPage);
+        }
+
+
+    }
+}
